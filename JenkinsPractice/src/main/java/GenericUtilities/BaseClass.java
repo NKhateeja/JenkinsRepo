478644213654f388dfb94a1088e76f759e2af2b4
NKhateeja <khateejaaf@gmail.com>
2023-06-02 14:33:31 +0530

    commit7

--- JenkinsPractice/src/main/java/GenericUtilities/BaseClass.java
+++ JenkinsPractice/src/main/java/GenericUtilities/BaseClass.java
@@ -1,35 +1,119 @@
 package GenericUtilities;
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class BaseClass {
-	public WebDriver driver;
-	static {
-		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
-	}
 	
+	
+	//creating objects for utility class
+	public FileUtilities Fut=new FileUtilities();
+	public ExcelUtilities Eut=new ExcelUtilities();
+	public WebDriverUtility1 Wut=new WebDriverUtility1();
+	public JavaUtility Jut=new JavaUtility();
+	
+	public  static WebDriver driver;
+	
+	
+	//giving connection to DB
 	@BeforeSuite
-	public void config_BS() throws InterruptedException {
-		System.out.println("before suite");
-		Thread.sleep(3000);
+	public void confid_BS() throws Throwable {
+		Thread.sleep(5000);
+		System.out.println("-connection to database-");
 	}
-	@BeforeClass
-	public void config_BC() {
+	
+	
+	//closing the connection
+	@AfterSuite 
+	public void confid_AS() {
+		System.out.println("-close connection to database-");
+	}
+	
+	
+	//launch the browser
+	//@Parameters("Browser")
+	@BeforeClass 
+	public void config_BC() throws Throwable {
 		
-		//WebDriverManager.chromiumdriver().create();
-		System.out.println("Befor class");
-		driver=new ChromeDriver();
+		System.out.println("Before class executing");
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "./driver/geckodriver.exe");
+		
+		String Browser = Fut.getPropertyKeyValue("browser");
+		
+		if(Browser.equalsIgnoreCase("chrome")) {
+			
+			driver=new ChromeDriver();
+		}
+		else if(Browser.equalsIgnoreCase("firefox")) {
+			//WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+		else if(Browser.equalsIgnoreCase("edge")) {
+			//WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+		}
+		else {
+			driver=new ChromeDriver();
+		}
+		
+		//maximizing the browser
+		Wut.maximizeWindow(driver);
+		
+		//giving implicit wait
+		Wut.waitTillPageGetsLoad(driver);
 		
 	}
 	
-	@BeforeMethod
-	public void config_BM() {
-		driver.get("http://rmgtestingserver/domain/Online_Banking_System/");
+	
+	//close the browser
+	@AfterClass 
+	public void confid_AC() {
+		
+		driver.close();
+		System.out.println("After class executed");
 	}
+	
+	
+	//login to the application
+	@BeforeMethod 
+	public void config_BM() throws Throwable {
+		
+		System.out.println("Before method executing");
+		String Url = Fut.getPropertyKeyValue("url");
+		driver.get(Url);
+		
+	}
+	
+	
+	//logout to the application
+	@AfterMethod 
+	public void confid_AM() {
+		System.out.println("After method executed");
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+

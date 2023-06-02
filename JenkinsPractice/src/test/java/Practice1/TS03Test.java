16a0328b9de2d6e8471bb7733724d7a67b578786
NKhateeja <khateejaaf@gmail.com>
2023-06-02 12:53:41 +0530

    commit

--- JenkinsPractice/src/test/java/Practice1/TS03Test.java
+++ JenkinsPractice/src/test/java/Practice1/TS03Test.java
@@ -5,8 +5,13 @@ import org.testng.annotations.Test;
 
 public class TS03Test {
 
-	@Test
+	@Test(invocationCount = 2)
 	public void tc03() {
 		Reporter.log("test case 03", true);
+	}
+	
+	@Test(priority = -1)
+	public void tc04() {
+		System.out.println("test case 04");
 	}
 }

5f4e482f0c3fe29d6404692ae1be6053322705d9
NKhateeja <khateejaaf@gmail.com>
2023-06-02 14:18:15 +0530

    commit6

--- JenkinsPractice/src/main/java/GenericUtilities/BaseClass.java
+++ JenkinsPractice/src/main/java/GenericUtilities/BaseClass.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -12,10 +13,17 @@ public class BaseClass {
 	static {
 		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
 	}
+	
+	@BeforeSuite
+	public void config_BS() throws InterruptedException {
+		System.out.println("before suite");
+		Thread.sleep(3000);
+	}
 	@BeforeClass
 	public void config_BC() {
 		
 		//WebDriverManager.chromiumdriver().create();
+		System.out.println("Befor class");
 		driver=new ChromeDriver();
 		
 	}
